463f673caf69fed6b6285b9288a8f894fb795e2e
Andres <Andres@MacBook-Pro-de-Andre.local>
2021-05-24 18:23:24 -0500

    started A* algorithm

--- PathFinder.java
+++ PathFinder.java
@@ -2,12 +2,59 @@ import java.util.ArrayList;
 
 public class PathFinder {
 
-    private ArrayList<Node> borders;
+    private Grid grid;
+    private int gHeight,
+                gWidth;
+    private int size;
 
-    public PathFinder(){
+    private ArrayList<Node> borders, 
+                            opened, 
+                            closed, 
+                            path;
+    private Node startNode, 
+                 endNode, 
+                 neighbor;
+    private boolean pathFound, 
+                    pathNotFound;
+
+    public PathFinder(Grid grid){
+        this.grid = grid;
+        this.gWidth = grid.getWidth();
+        this.gHeight = grid.getHeight();
+
         this.borders = new ArrayList<Node>();
+        this.opened = new ArrayList<Node>();
+        this.closed = new ArrayList<Node>();
+        this.path = new ArrayList<Node>();
     }
 
+    public void runProgram(Node startNode, Node endNode){
+        if(startNode != null && endNode != null){
+            this.findPath(startNode);
+        }
+    }
+
+    public void findPath(Node current){
+        //For loops to create adyacent nodes if there are inside the grid
+        for(int i=0, dragY = -25; i < 3; i++, dragY += 25){
+            for(int j=0, dragX = -25; j < 3; j++, dragX += 25){
+                if(i == j || Math.abs(i-j) > 1){
+                    continue;
+                }
+                int neighborXPos = dragX + current.getX();
+                int neighborYPos = dragX + current.getY();
+
+                //Nodes that are inside of the coordinates
+                if(neighborXPos >= 0 && neighborXPos <= this.gWidth && 
+                        neighborYPos >= 0 && neighborYPos <= this.gHeight){
+                        // System.out.println((neighborXPos + " " + neighborYPos);
+                        //Need to check if node is not border to add
+                        
+                        
+                }
+            }
+        }
+    }
 
     public void addBorder(Node border){
         borders.add(border);
@@ -35,8 +82,9 @@ public class PathFinder {
 			}
 		}
 		return Location;
-	}
-
-
+	}   
     
+    public ArrayList<Node> getPathList(){
+        return this.path;
+    }
 }
